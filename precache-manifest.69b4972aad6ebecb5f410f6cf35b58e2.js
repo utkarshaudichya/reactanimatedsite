8f48ee5e4e476975926b8adf5c9043a4d0f44f3c
Utkarsh Audichya <47419268+utkarshaudichya@users.noreply.github.com>
2020-09-04 15:52:24 +0530

    Updates

---
+++ precache-manifest.69b4972aad6ebecb5f410f6cf35b58e2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958824a66555ca1df48ba1916af1982a",
+    "url": "/reactanimatedsite/index.html"
+  },
+  {
+    "revision": "9a89e22083d7c44a0f03",
+    "url": "/reactanimatedsite/static/css/2.aff18e8f.chunk.css"
+  },
+  {
+    "revision": "32b8a106cf2b39c3802c",
+    "url": "/reactanimatedsite/static/css/main.a8a56488.chunk.css"
+  },
+  {
+    "revision": "9a89e22083d7c44a0f03",
+    "url": "/reactanimatedsite/static/js/2.d8345754.chunk.js"
+  },
+  {
+    "revision": "37e84efbe36a4d27b31676a661d68f91",
+    "url": "/reactanimatedsite/static/js/2.d8345754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b8a106cf2b39c3802c",
+    "url": "/reactanimatedsite/static/js/main.bc4a438b.chunk.js"
+  },
+  {
+    "revision": "a16434db43f9f24f2e68",
+    "url": "/reactanimatedsite/static/js/runtime-main.3f93f95a.js"
+  },
+  {
+    "revision": "3ec664826889b0cca41376cfb420e624",
+    "url": "/reactanimatedsite/static/media/about.3ec66482.png"
+  },
+  {
+    "revision": "5bbfe9d19cfe890b588a2d621a5e5a14",
+    "url": "/reactanimatedsite/static/media/android.5bbfe9d1.jpg"
+  },
+  {
+    "revision": "500e797276d342519b9fe281dbe86176",
+    "url": "/reactanimatedsite/static/media/app.500e7972.jpg"
+  },
+  {
+    "revision": "033c014af82b3fd2e31c36b25bd41ff9",
+    "url": "/reactanimatedsite/static/media/db.033c014a.jpg"
+  },
+  {
+    "revision": "a8afa7279d5f0c8f9289848779192338",
+    "url": "/reactanimatedsite/static/media/home2.a8afa727.png"
+  },
+  {
+    "revision": "03ee7b1dd2b48acb87a53afc4e519e12",
+    "url": "/reactanimatedsite/static/media/seo.03ee7b1d.jpg"
+  },
+  {
+    "revision": "999b83fb9e213b89aee90250819fe2a0",
+    "url": "/reactanimatedsite/static/media/soft.999b83fb.jpg"
+  },
+  {
+    "revision": "dd3ec9edcb1763cbb7e9a7287f0e4df2",
+    "url": "/reactanimatedsite/static/media/web2.dd3ec9ed.jpg"
+  }
+]);
